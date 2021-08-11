547f44aa92b9f37dfedbe6bd9ff03eafd8dc094b
fatmamohsen <319190400@sha.edu.eg>
2021-08-11 16:55:45 +0200

    add js file and videos folder

---
+++ main.js
@@ -0,0 +1,69 @@
+function button(){
+    var moreText= document.getElementById("more");
+    
+    if(moreText.style.display== "none"){
+        moreText.style.display="block"
+
+    }
+    else{
+        moreText.style.display="none"
+    }
+}
+function button1(){
+    var moreText= document.getElementById("more1");
+    
+    if(moreText.style.display== "none"){
+        moreText.style.display="block"
+
+    }
+    else{
+        moreText.style.display="none"
+    }
+}
+function button2(){
+    var moreText= document.getElementById("more2");
+    
+    if(moreText.style.display== "none"){
+        moreText.style.display="block"
+
+    }
+    else{
+        moreText.style.display="none"
+    }
+}
+function button3(){
+    var moreText= document.getElementById("more3");
+    
+    if(moreText.style.display== "none"){
+        moreText.style.display="block"
+
+    }
+    else{
+        moreText.style.display="none"
+    }
+}
+function button4(){
+    var moreText= document.getElementById("more4");
+    
+    if(moreText.style.display== "none"){
+        moreText.style.display="block"
+
+    }
+    else{
+        moreText.style.display="none"
+    }
+}
+function button5(){
+    var moreText= document.getElementById("more5");
+    
+    if(moreText.style.display== "none"){
+        moreText.style.display="block"
+
+    }
+    else{
+        moreText.style.display="none"
+    }
+}
+
+
+
