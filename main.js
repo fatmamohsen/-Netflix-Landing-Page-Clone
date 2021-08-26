e27c4758ef929315e1ffad74678d010941ce0dad
fatmamohsen <319190400@sha.edu.eg>
2021-08-26 21:12:39 +0200

    modified the three files

--- main.js
+++ main.js
@@ -1,69 +1,15 @@
-function button(){
-    var moreText= document.getElementById("more");
-    
-    if(moreText.style.display== "none"){
-        moreText.style.display="block"
-
-    }
-    else{
-        moreText.style.display="none"
-    }
-}
-function button1(){
-    var moreText= document.getElementById("more1");
-    
-    if(moreText.style.display== "none"){
-        moreText.style.display="block"
-
-    }
-    else{
-        moreText.style.display="none"
-    }
-}
-function button2(){
-    var moreText= document.getElementById("more2");
-    
-    if(moreText.style.display== "none"){
-        moreText.style.display="block"
-
-    }
-    else{
-        moreText.style.display="none"
-    }
-}
-function button3(){
-    var moreText= document.getElementById("more3");
-    
-    if(moreText.style.display== "none"){
-        moreText.style.display="block"
-
-    }
-    else{
-        moreText.style.display="none"
-    }
-}
-function button4(){
-    var moreText= document.getElementById("more4");
-    
-    if(moreText.style.display== "none"){
-        moreText.style.display="block"
-
-    }
-    else{
-        moreText.style.display="none"
-    }
-}
-function button5(){
-    var moreText= document.getElementById("more5");
-    
-    if(moreText.style.display== "none"){
-        moreText.style.display="block"
-
-    }
-    else{
-        moreText.style.display="none"
-    }
-}
-
-
+const questions=document.querySelectorAll(".list-item");
+const answer=document.querySelector(".answer");
+questions.forEach(function(questionBtn){
+    const btn=questionBtn.querySelector(".question");
+        
+     btn.addEventListener("click",function(){
+         questions.forEach(function(item){
+             if(item!==questionBtn){
+                item.classList.remove("show-text");
+             }
+         })
+         questionBtn.classList.toggle("show-text");
+     })
+})
 
